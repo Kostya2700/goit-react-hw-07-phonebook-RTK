be13bcbd2a59914e7e631c51aae9eb09ecc623a6
kostya <kostizavali@gmail.com>
2022-11-11 22:07:47 +0200

    phone

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -6,21 +6,18 @@ import Filter from 'components/Filter/Filter';
 import ListItem from 'components/ListItem/ListItem';
 import { Title } from 'components/Title/Title';
 import { useState } from 'react';
+const KEYLOCAL = 'contacts';
 function Phonebook() {
   const [contacts, setContacts] = useState(() =>
-    JSON.parse(window.localStorage.getItem('contacts') ?? [])
+    JSON.parse(window.localStorage.getItem(KEYLOCAL) ?? [])
   );
   const [filter, setFilter] = useState('');
 
   const deleteContact = id => {
     setContacts(contacts.filter(contact => contact.id !== id));
-    // this.setState(prev => ({
-    //   contacts: prev.contacts.filter(contact => contact.id !== id),
-    // }));
   };
   const addItem = (names, numbers) => {
     const normalizedFilter = names.toLowerCase();
-    // console.log(contacts);
     const checkByName = contacts.find(
       contact => contact.names.toLowerCase() === normalizedFilter
     );
@@ -35,49 +32,21 @@ function Phonebook() {
       };
 
       setContacts([contact, ...contacts]);
-      // this.setState(({ contacts }) => ({
-      //   contacts: [contact, ...contacts],
-      // }));
     }
   };
-  // const formSubmitChanging = (name, number) => {
-  //   setContacts(addItem(name, number));
-  //   // this.setState(this.addItem(name, number));
-  // };
+
   const changeFilter = e => {
     setFilter(e.currentTarget.value);
-    // this.setState({ filter: e.currentTarget.value });
   };
   const getFilterContact = () => {
     return contacts.filter(contact =>
       contact.names.toLowerCase().includes(filter.toLowerCase())
     );
   };
-  // useEffect(() => {
-  //   console.log('first');
-  //   const local = localStorage.getItem('contacts');
-  //   const localParse = JSON.parse(local);
-  //   console.log(localParse);
-  //   if (localParse) {
-  //     setContacts(localParse);
-  //   }
-  // }, []);
-  // componentDidMount() {
-  //   const local = localStorage.getItem('contacts');
-  //   const localParse = JSON.parse(local);
-  //   if (localParse) {
-  //     this.setState({ contacts: localParse });
-  //   }
-  // }
+
   useEffect(() => {
-    console.log(contacts);
-    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    window.localStorage.setItem(KEYLOCAL, JSON.stringify(contacts));
   }, [contacts]);
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (this.state.contacts !== prevState.contacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-  //   }
-  // }
 
   return (
     <>
